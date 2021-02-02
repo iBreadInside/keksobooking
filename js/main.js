cfee7664b52e94e76f494d709c0042b779aea354
Владимир <nekro199107@gmail.com>
2021-02-03 00:23:40 +0300

    Добавил функцию для случайного дробного числа

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+
+
 // Случайное целое число из заданного диапазона
-let getRandomInt = (min, max) => {
-  return (min < 0 || min >= max) ? 'Введен некорректный диапазон' : Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomInt = (min, max) => {
+  return (min < 0 || max < 0) ? 'Доступны только положительные значения' :
+    (min >= max) ? (
+      [min, max] = [max, min],
+      Math.floor(Math.random() * (max - min + 1)) + min) :
+      Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInt(3, 15)
+getRandomInt(4, 3)
+
+// Случайное число с плавающей точкой
+const getRandomFloat = (min, max, decimal) => {
+  return (min < 0 || max < 0) ? 'Доступны только положительные значения' :
+    (min >= max) ? (
+      [min, max] = [max, min],
+      ((Math.random() * (max - min)) + min).toFixed(decimal)) :
+      (Math.random() * (max - min) + min).toFixed(decimal);
+}
+
+getRandomFloat(4.2, 4.1, 3)
