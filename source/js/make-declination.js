113dff61cd160e71aaa2fedc38f13f69bd5767b7
Владимир <nekro199107@gmail.com>
2021-03-22 01:20:10 +0300

    B17 fix

--- source/js/make-declination.js
+++ source/js/make-declination.js
@@ -4,6 +4,6 @@ export const makeDeclination = (num, words) => {
   const num1 = num % 10;
   if (num > 10 && num < 20) { return words[2]; }
   if (num1 > 1 && num1 < 5) { return words[1]; }
-  if (num1 == 1) { return words[0]; }
+  if (num1 === 1) { return words[0]; }
   return words[2];
 };
