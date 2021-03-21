82b8c5425b108dc06336c06def86550fe10fd8d6
Владимир <nekro199107@gmail.com>
2021-03-21 21:56:48 +0300

    Gitignore fixes

---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+import {adFormInnerLinks} from './ad-form.js';
+import {toggleAdFormState, disableFilterState} from './forms-disabled.js';
+import {mapFunctions} from './map.js';
+
+toggleAdFormState();
+disableFilterState();
+adFormInnerLinks();
+mapFunctions();
